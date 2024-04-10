167a62cf2a9d74bb0e764fd753997385b9638330
memmedlileman <memmedileman02@gmail.com>
2024-04-10 13:52:59 +0400

    task21

---
+++ app.js
@@ -0,0 +1,21 @@
+// const coins = [
+//     { name: "500 manat", value: 500.00 },
+//     { name: "200 manat", value: 200.00 },
+//     { name: "100 manat", value: 100.00 },
+//     { name: "50 manat", value: 50.00 },
+//     { name: "20 manat", value: 20.00 },
+//     { name: "10 manat", value: 10.00 },
+//     { name: "5 manat", value: 5.00 },
+//     { name: "1 manat", value: 1.00 }
+// ];
+
+function hesabla() {
+    const moneyInput = document.getElementById("money");
+    const money = parseFloat(moneyInput.value);
+
+   if( ){
+
+   }
+
+    // document.getElementById("result").innerHTML = result;
+}
